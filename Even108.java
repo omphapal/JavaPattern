0b079f67e02b40cc4af91cf3a024ad47ad084acc
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-10 14:18:38 +0530

    Add Even108 class to print pattern of numbers

---
+++ Even108.java
@@ -0,0 +1,19 @@
+package feb;
+
+public class Even108 {
+	public static void main(String[] args) {
+		
+	
+	for(int i=0; i<5; i++) {
+		for(int j=0; j<5; j++) {
+	
+			if( i==j) {
+				System.out.print(j);
+			}else {
+			
+				System.out.print("0");
+				}
+		}		System.out.println();	
+	}	
+}
+}
