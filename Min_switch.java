dcf3c1016cc85940329e88d12251a423e94a9c36
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:19:45 +0530

    Add Min_switch class with switch-case logic
    Implement switch-case structure to handle number ranges.

---
+++ Min_switch.java
@@ -0,0 +1,24 @@
+package feb2;
+
+public class Min_switch {
+
+	public static void main(String[] args) {
+		
+		int num=-9;
+		
+		switch(num) {
+		case 1-5:
+			System.out.println("www.Gmail.Com");
+		break;
+		case 6-15:
+			System.out.println("www.google.com");
+		break;
+		
+		default:
+			System.out.println("Wrong option");
+			break;
+			
+		}
+		
+	}
+}
