a664e249317f86aad3c52fbe68a17675f039b53f
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:20:15 +0530

    Create Patter.java

---
+++ Patter.java
@@ -0,0 +1,21 @@
+package feb2;
+
+public class Patter {
+
+	public static void main(String[] args) {
+		
+		int i=1;
+		int j=1;
+		for(i=1; i<=10; i++) {
+			
+			for(j=10; j>=1; j--) {
+				System.out.print(j);
+			}
+			System.out.println();
+				
+			}
+		}
+
+	
+
+}
