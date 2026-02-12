4a01a098b814623e51d2763de3482834fc1356e8
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:16:54 +0530

    Add Loop1 class with nested loops for printing

---
+++ Loop1.java
@@ -0,0 +1,20 @@
+package feb_5;
+
+public class Loop1 {
+
+	public static void main(String[] args) {
+		int j=0;
+		for(int i = 1; i<=5; i++) {
+			for( j=5; j>=1; j--) {
+				if(j<=i) {
+				
+				System.out.print(i);
+			}else{
+				System.out.print(1);
+			
+			}
+				
+				}System.out.println();
+		}
+	}
+}
