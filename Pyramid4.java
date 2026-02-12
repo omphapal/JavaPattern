874759f72856de7a2fa7ccd887646f1dd8fa8bf5
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:33:59 +0530

    Add Pyramid4.java for printing pyramid patterns
    Implemented a pyramid pattern printing program.

---
+++ Pyramid4.java
@@ -0,0 +1,20 @@
+package feb7;
+
+public class Pyramid4 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n=5;
+		for(int i=1; i<=5; i++) {
+			
+			for(int s=1; s<=n-i; s++) {
+			System.out.print(" ");
+		}
+		for(int j=1; j<=i; j++) {
+			System.out.print("* ");
+		}System.out.println();
+	}
+	}
+
+}
