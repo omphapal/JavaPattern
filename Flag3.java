18ca672b10ac2810fcdf710d594f2a682ab01976
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:26:55 +0530

    Create Flag3.java

---
+++ Flag3.java
@@ -0,0 +1,31 @@
+package feb6;
+
+public class Flag3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char i;
+		char j;
+		char k;
+		char l;
+		
+		
+		for(i='A'; i<='F'; i++) {
+			for(j=i; j==i; j--) {
+				System.out.print(j);
+			}
+				System.out.println();
+			}
+		
+//		for(k='A'; k<='f'; k++) {
+//			for(l='A'; l<=k ; l++) {
+//				System.out.print(l);	
+//}System.out.println();
+
+
+
+		
+	
+	}
+
+}
