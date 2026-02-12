1dbdf408f4135cb3a7393afd9033b6a3a139367b
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:25:42 +0530

    Add Alphabate2.java to print letters and numbers

---
+++ Alphabate2.java
@@ -0,0 +1,23 @@
+package feb6;
+
+public class Alphabate {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+char c;
+		for(c='A'; c<='z'; c++) {
+			System.out.print(c+" ");
+		}
+		System.out.println();
+	
+	for(c=65; c<=90; c++) {
+		System.out.print(c+" ");
+	}
+	for(int i=1; i<=10; i++) {
+		System.out.println(i);
+	}
+	c=32;
+	System.out.println("space"+c+"spcae"+c+"spzce");
+
+}
+}
