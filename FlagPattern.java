375f8bd07e01373b221070edf403e5161ffa8eea
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:27:21 +0530

    Add FlagPattern class for printing flag pattern
    Implement a flag pattern printing program using nested loops.

---
+++ FlagPattern.java
@@ -0,0 +1,28 @@
+package feb6;
+
+public class FlagPattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char i;
+		char j;
+		char k;
+		char l;
+		for(i='A'; i<='E'; i++) {
+			for(j='A'; j<=i; j++) {
+				System.out.print(i);
+			}
+				System.out.println();
+			}
+		
+				for(k='D'; k>='A'; k--) {
+					for(l='A'; l<=k ; l++) {
+						System.out.print(k);	
+		}System.out.println();
+				
+			}
+			
+		}
+	}
+	
