ef86c9c982606c7aa8195ff7372a9b198ec11893
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:26:11 +0530

    Add Alphabateloop class to print alphabet pattern

---
+++ Alphabateloop.java
@@ -0,0 +1,15 @@
+package feb6;
+
+public class Alphabateloop {
+
+	public static void main(String[] args) {
+		char x;
+		char y;
+		for(x='A'; x<='E'; x++) {
+			for(y='A'; y<='E'; y++ ) {
+				System.out.print(y);
+			}
+			System.out.println();
+		}
+	}
+}
