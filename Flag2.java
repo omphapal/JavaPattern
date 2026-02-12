3f73a5dd5a6739ae8155279403f565f1e763681a
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:26:34 +0530

    Add Flag2 class to print character patterns
    Implement a program to print a pattern using nested loops.

---
+++ Flag2.java
@@ -0,0 +1,28 @@
+package feb6;
+
+public class Flag2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char i;
+		char j;
+		char k;
+		char l;
+		for(i='A'; i<='E'; i++) {
+			for(j='A'; j<=i; j++) {
+				System.out.print(j);
+			}
+				System.out.println();
+			}
+		
+				for(k='D'; k>='A'; k--) {
+					for(l='A'; l<=k ; l++) {
+						System.out.print(l);	
+		}System.out.println();
+				
+			}
+			
+		}
+	
+	}
+
