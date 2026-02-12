892d491e929829d80a5a24de9783a62847564768
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:27:44 +0530

    Add LoopAlphabate class to print alphabet pattern

---
+++ LoopAlphabate.java
@@ -0,0 +1,15 @@
+package feb6;
+
+public class LoopAlphabate {
+
+	public static void main(String[] args) {
+		int i,j;
+		for(i=1; i<=5; i++) {
+			char ch='A';
+			for(j=1; j<=5; j++) {
+				System.out.print(ch);
+				ch++;
+			}System.out.println();
+		}
+		}
+}
