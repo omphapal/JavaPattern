2320df37540d2769920b4d83fefd8da8f6e7e192
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:29:34 +0530

    Create Pattern1.java

---
+++ Pattern1.java
@@ -0,0 +1,17 @@
+package feb7;
+
+public class Pattern1 {
+
+	public static void main(String[] args) {
+		for(int i=0; i<=9; i++){
+			for(int j=0; j<=9; j++) {
+				for(int l=0; l<=3; l++) {
+				System.out.print(j);
+				
+				} 
+				}System.out.println();
+			
+			}
+	
+	}
+}
