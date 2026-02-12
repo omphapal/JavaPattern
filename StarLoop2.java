106b8b12e0fdbd558d95a0417ac9c06b6f4d69e9
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:19:13 +0530

    Add StarLoop2.java for printing star patterns

---
+++ StarLoop2.java
@@ -0,0 +1,16 @@
+package feb_5;
+
+public class StarLoop2 {
+	public static void main(String[] args) {
+		int j=0;
+		for(int i = 5; i>=1; i--) {
+			for( j=1; j<=5; j++) {
+				
+				if(j>=i && j<i) {
+				System.out.print(i);
+				}else {
+				}System.out.print(5);
+			}System.out.println();
+			
+	}
+}}
