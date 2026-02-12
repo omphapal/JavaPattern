9e244ed5dd5997d5a2d243f75b8efad3e09284a8
Omkar Phapal <144727650+omphapal@users.noreply.github.com>
2026-02-12 07:21:19 +0530

    Add Pattern2 class to print numbers in a grid

---
+++ Pattern2.java
@@ -0,0 +1,19 @@
+package feb2;
+
+public class Pattern2 {
+
+	public static void main(String[] args) {
+
+		int i;
+		int j;
+		for(i=1; i<=10; i++) {
+			
+			for(j=1; j<=10; j++) {
+				System.out.print(j);
+			}
+			System.out.println();
+		}
+
+	}
+
+}
